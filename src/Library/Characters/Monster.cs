6ab845a7881ec993a242a910f7d6ac1d6aa14512
facumar93 <53872293+facumar93@users.noreply.github.com>
2020-05-08 18:16:27 -0300

    Agrego a los malos

--- src/Library/Characters/Monster.cs
+++ src/Library/Characters/Monster.cs
@@ -1,23 +1,27 @@
 using System.Collections.Generic;
 namespace RoleplayGame
 {
-    public class Hero : Character
+    public class Monster : Character
     {
-        public Hero(string name) : base(name)
+        public Monster(string name) : base(name)
         {
             this.Pv = Pv;
         }
+
         public int Pv { get; set; }
+
         public override int AttackValue => base.AttackValue;
 
         public override int DefenseValue => base.DefenseValue;
 
-        public void MorePv(int deadPv)
+        public int ChechHealth()
         {
-            if ( deadPv > 0)
+            if ( this.Health <= 0 )
             {
-                this.Pv += deadPv;     
+                return this.Pv;
             }
+            else
+                return 0;
         }
 
         public override bool Equals(object obj)
